4fa42b1224231dc65a248746b8b1d6b9a497c30a
daveslusher-sn <dave.slusher@servicenow.com>
2019-01-18 10:33:54 -0500

    Blog commenting work

--- static/js/stats.js
+++ static/js/stats.js
@@ -1,23 +1,77 @@
+function getPage() {
+    var p = getUrlParameter("p");
+    if (p) {
+        if (! p.endsWith("index.html")) {
+            if (p.endsWith("/")) {
+                p = p + "index.html";
+            } else {
+                p = p + "/index.html";
+            }
+        }
+        return p;
+    }
+}
+
 $(function() {
     $(document).ready(function() {
         console.log("Inside JQuery's ready function");
-        var p = getUrlParameter("p");
-        if (p) {
-            if (! p.endsWith("index.html")) {
-                if (p.endsWith("/")) {
-                    p = p + "index.html";
-                } else {
-                    p = p + "/index.html";
-                }
-            }
-            $.get('/api/x_snc_devblog/v1/vfs/getCount?p=' + p , 
-                function(response) {
-                    $('#read-count').html("Read Count: " + response.result.count); 
-                }
-            ); 
-        }
+        var p = getPage();
+        loadReadCount(p);
+        loadComments(p);
     });
 })
+
+function loadReadCount(p) {
+    $.get('/api/x_snc_devblog/v1/vfs/getCount?p=' + p , 
+    function(response) {
+        console.log("In read count "+ JSON.stringify(response, null, 4));
+        console.log(response);
+        $('#read-count').html("Read Count: " + response.result.count); 
+    }
+); 
+}
+
+function loadComments(p) {
+    $.get('/api/x_snc_devblog/v1/vfs/getComments?p=' + p , 
+    function(response) {
+        var commentBlock = "";
+        console.log("response = " + JSON.stringify(response, null, 4));
+        console.log(response);
+        response.result.comments.forEach( function(comment){
+            console.log("in loop with "+ comment.text);
+            commentBlock += "<li>"+comment.time + " <br />" + comment.author + " <br />" + comment.text+"</li>";
+        } 
+
+        );
+        $('#comments').html( commentBlock); 
+    }
+); 
+}
+
+function submitComment() {
+    console.log("Clicked submit");
+    var p = getPage();
+    var text = $('#comment-text').val();
+    var json = {};
+    json.path = p;
+    json.text = text;
+    var jsonString = JSON.stringify(json);
+    console.log(jsonString);
+    jQuery.ajax ({
+        url: '/api/x_snc_devblog/v1/vfs/postComment',
+        type: "POST",
+        data: jsonString,
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function(){
+            console.log("Submit post worked!");
+            loadComments(p);
+        }
+    });
+
+
+
+}
 
 var getUrlParameter = function getUrlParameter(sParam) {
     var parameters = window.location.search.substring(1);
--- static/js/usermenu.js
+++ static/js/usermenu.js
@@ -21,16 +21,64 @@ usermenu.init = function(properties) {
             }); 
         });
         
-        $.get('/getSession.do', function(response) {
+        this.getSession = function(token) {
+            if (token == "") {
+                try {
+                    token = window.localStorage.getItem('X-UserToken');
+                } catch (e){
+                    console.log("Error: "+e);
+                }
+            }
+
+            $.ajax('https://devportaldev.service-now.com/devportal.do?sysparm_data=\{%22action%22:%22dev.user.session%22,%22data%22:\{\}\}', {
+                headers: {'X-UserToken': token},
+                401: this.getSessionHandler401,
+                200: this.getSessionHandler200
+            });
+
+
+        };
+
+        this.getSessionHandler200 = function(response) {
             var displayName = response.display_name; 
             if (displayName == 'Guest') {
                 $('.current-user').hide();
                 $('.login').each(function() {
                     $(this).removeClass('menu-hidden');
                 });
+                $("#comment-button").prop("disabled",true);
             } else {
                 $('.current-user').html(displayName);
+                $("#comment-button").prop("disabled",false);
             }
+        };
+
+        this.getSessionHandler401 = function(response) {
+            var headers = response.headers; 
+            var token = headers.X-UserToken-Response;
+            try {
+                window.localStorage.setItem('X-UserToken', token);
+            } catch {
+            }
+            this.getSession(token);
+        };
+
+        this.getSession();
+
+/*        $.get('/getSession.do', function(response) {
+            var displayName = response.display_name; 
+            if (displayName == 'Guest') {
+                $('.current-user').hide();
+                $('.login').each(function() {
+                    $(this).removeClass('menu-hidden');
+                });
+                $("#comment-button").prop("disabled",true);
+            } else {
+                $('.current-user').html(displayName);
+                $("#comment-button").prop("disabled",false);
+            }            
         }); 
+*/
+
     });
 }; 
