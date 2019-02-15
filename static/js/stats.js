a160cf0d4204943f56920c44cbe9c8592acfe9e7
daveslusher-sn <dave.slusher@servicenow.com>
2019-02-15 16:39:31 -0500

    Changes for blog updates, including commenting scripts + top post fix

--- static/js/stats.js
+++ static/js/stats.js
@@ -1,23 +1,93 @@
+function getPage() {
+    var p = getUrlParameter("p") || $("#current-slug").val();
+    console.log("in getPage, p = " + p);
+    if (p) {
+        if (! p.endsWith("index.html")) {
+            if (p.endsWith("/")) {
+                p = p + "index.html";
+            } else {
+                p = p + "/index.html";
+            }
+        }
+        return p;
+    }
+}
+
 $(function() {
     $(document).ready(function() {
-        console.log("Inside JQuery's ready function");
-        var p = getUrlParameter("p");
-        if (p) {
-            if (! p.endsWith("index.html")) {
-                if (p.endsWith("/")) {
-                    p = p + "index.html";
-                } else {
-                    p = p + "/index.html";
-                }
-            }
-            $.get('/api/x_snc_devblog/v1/vfs/getCount?p=' + p , 
-                function(response) {
-                    $('#read-count').html("Read Count: " + response.result.count); 
-                }
-            ); 
-        }
+//        console.log("Inside JQuery's ready function");
+        var p = getPage();
+        loadReadCount(p);
+        loadComments(p);
     });
 })
+
+function loadReadCount(p) {
+    $.get('/api/x_snc_devblog/v1/vfs/getCount?p=' + p , 
+    function(response) {
+        $('#read-count').html("Read Count: " + response.result.count); 
+    }
+); 
+}
+
+function loadComments(p) {
+    $.get('/api/x_snc_devblog/v1/vfs/getComments?p=' + p , 
+    function(response) {
+        var commentBlock = "<ol>";
+//        console.log("response = " + JSON.stringify(response, null, 4));
+//       console.log(response);
+        response.result.comments.forEach( function(comment){
+//            console.log("in loop with "+ comment.text);
+            commentBlock += "<li>";
+            commentBlock += "<div class=\"commenter\">"+comment.author + "</div>";
+            commentBlock += "<div class=\"time\">"+comment.time + "</div>";
+            commentBlock += "<div class=\"text\">"+comment.text+"</div>";
+            commentBlock += "</li>";
+        } 
+        );
+        commentBlock += "</ol>"
+        $('#comments').html( commentBlock); 
+    }
+); 
+}
+
+function submitComment(token) {
+//    console.log("Clicked submit");
+    var p = getPage();
+    var text = $('#comment-text').val();
+    var json = {};
+    json.path = p;
+    json.text = text;
+    var jsonString = JSON.stringify(json);
+    if (!token) {
+        token = window.localStorage.getItem('X-UserToken');
+    }
+//    console.log("In submitComment() function, token = " + token);
+//    console.log("In submitComment() function, sessionID = " + sessionID);
+    
+
+    jQuery.ajax ({
+        url: '/api/x_snc_devblog/v1/vfs/postComment',
+        beforeSend: function(request) {
+            request.setRequestHeader("X-UserToken", token);
+        },
+        type: "POST",
+        data: jsonString,
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function(){
+//            console.log("Submit post worked!");
+            loadComments(p);
+            $('#comment-text').val('');
+        },
+        error: function(xhr, error_text, statusText) {
+//            console.log("In error handler");
+            var token = xhr.getResponseHeader('X-UserToken-Response');
+            submitComment(token);
+        }
+    });
+
+}
 
 var getUrlParameter = function getUrlParameter(sParam) {
     var parameters = window.location.search.substring(1);
--- static/js/usermenu.js
+++ static/js/usermenu.js
@@ -1,35 +1,111 @@
-usermenu = {}; 
-usermenu.init = function(properties) {
-    $(document).ready(function() {
+usermenu = {};
 
-        $('.current-user').click(function(e) {
+usermenu.handleLoginStatus = function (response) {
+    var displayName = response.display_name; 
+    var sessionID = response.session_id;
+    if (sessionID != "SYSTEM") {
+        usermenu.session_id = sessionID;
+        window.localStorage.setItem('SessionID', sessionID);                   
+//        console.log("Set usermenu.session_id to " + sessionID);
+    }
+
+    if (displayName == 'Guest') {
+        $('.current-user').hide();
+        $('.login').each(function() {
+            $(this).removeClass('menu-hidden');
+        });
+        $("#comment-button").prop("disabled",true);
+        $("#comment-button").hide();
+        $("#comment-text").hide();
+        $("#comment-login").show();
+    } else {
+        $('.current-user').html(displayName);
+        $("#comment-button").prop("disabled",false);
+        $("#comment-button").show();
+        $("#comment-text").show();
+        $("#comment-login").hide();
+        $("#comment-notice").html('Submitting comments as ' + displayName);
+
+    }       
+};
+
+usermenu.session_id = "";
+
+usermenu.init = function (properties) {
+    $(document).ready(function () {
+      
+        this.getSession = function (token) {
+//            console.log("Entering getSession with token=" + token);
+            if (token == undefined || token == "") {
+//                console.log("No token");
+                try {
+                    token = window.localStorage.getItem('X-UserToken');
+//                    console.log("Retrieved from local storage token: " + token);
+                } catch (e) {
+                    console.log("Error: " + e);
+                }
+            } else {
+  //              console.log("Token exists and is " + token);
+            }
+
+            var headers = {};
+
+            if (token == undefined || token == "") {
+//                console.log("No user token");
+            } else {
+                headers = { 'X-UserToken': token };
+//                console.log("adding X-UserToken to ajax call: " + token);
+            }
+
+            $.ajax('devportal.do?sysparm_data=\{%22action%22:%22dev.user.session%22,%22data%22:\{\}\}', {
+                headers: headers,
+        
+            })
+                .done(function (data, textStatus, jqXHR) {
+                    //console.log("Success handler: " + JSON.stringify(data) + " " + textStatus);
+                })
+                .fail(function (jqXHR, textStatus, errorThrown) {
+//                    console.log("Error handler: " + textStatus + " " + errorThrown);
+                })
+                .always(function (response, textStatus, jqXHR) { //;paf; see http://stackoverflow.com/a/19498463/2914328
+//                    console.log("jQuery version: " + $.fn.jquery);
+//                    console.log("arg1:", response);
+//                    console.log("arg2:", textStatus);
+//                    console.log("arg3:", jqXHR);
+                    var responseToken = jqXHR.getResponseHeader('X-UserToken') ||jqXHR.getResponseHeader('X-UserToken-Response') ;
+//                    console.log("X-UserToken = " + responseToken);
+                    if (token != responseToken) {
+                        // If we come here, we are getting a new token
+                        window.localStorage.setItem('X-UserToken', responseToken);
+//                        console.log("Set X-UserToken in local storage to " + responseToken);
+                    }
+//                    console.log("this =  " +JSON.stringify(this));
+                    usermenu.handleLoginStatus(response);
+                }
+                );
+        };
+        
+
+        $('.current-user').click(function (e) {
             e.stopPropagation();
-            var menu = $('.user-menu ul'); 
+            var menu = $('.user-menu ul');
             menu.toggleClass('menu-hidden');
 
-            $('body').on('click', function() {
+            $('body').on('click', function () {
                 menu.attr('class', 'menu-hidden');
             });
         });
 
-        $('.logout').click(function() {
-            $.get('/logout.do', function(response) {
-                var logoutURL = properties.okta + '/login/signout?fromURI='; 
-                logoutURL += 'https://' + window.location.host + '/app.do'; 
-                window.location.href = logoutURL; 
-            }); 
+
+        $('.logout').click(function () {
+            $.get('/logout.do', function (response) {
+                var logoutURL = properties.okta + '/login/signout?fromURI=';
+                logoutURL += window.location;
+                window.location.href = logoutURL;
+            });
         });
-        
-        $.get('/getSession.do', function(response) {
-            var displayName = response.display_name; 
-            if (displayName == 'Guest') {
-                $('.current-user').hide();
-                $('.login').each(function() {
-                    $(this).removeClass('menu-hidden');
-                });
-            } else {
-                $('.current-user').html(displayName);
-            }
-        }); 
-    });
-}; 
+  //      console.log("In main thread, right before calling getSession()");
+        this.getSession();
+    }
+    )
+}
