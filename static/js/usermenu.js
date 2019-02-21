bdea4b47ae7c1d14e998b8ef3a18f73f71c4f786
daveslusher-sn <dave.slusher@servicenow.com>
2019-02-21 16:23:45 -0500

    Fixing the failure to re-request token on devportalqa

--- static/js/usermenu.js
+++ static/js/usermenu.js
@@ -3,7 +3,7 @@ usermenu = {};
 usermenu.handleLoginStatus = function (response) {
     var displayName = response.display_name; 
 
-    if (displayName == 'Guest') {
+    if (!displayName || displayName == 'Guest') {
         $('.current-user').hide();
         $('.login').each(function() {
             $(this).removeClass('menu-hidden');
@@ -30,8 +30,15 @@ usermenu.init = function (properties) {
       
         this.getSession = function (token) {
             var headers = {};
+            var container = this;
 
             if (token == undefined || token == "") {
+                try {	
+                    token = window.localStorage.getItem('X-UserToken');	
+                    console.log("Retrieved from local storage token: " + token);	
+                } catch (e) {	
+                    console.log("Error: " + e);	
+                }
             } else {
                 headers = { 'X-UserToken': token };
             }
@@ -42,19 +49,29 @@ usermenu.init = function (properties) {
             })
             /* Leaving empty handlers but not actually doing anything. Add later if needed */
                 .done(function (data, textStatus, jqXHR) {
+                    console.log("Success handler: " + JSON.stringify(data) + " " + textStatus);
+                    usermenu.handleLoginStatus(data);
+
                 })
                 .fail(function (jqXHR, textStatus, errorThrown) {
+                    console.log("Error handler: " + textStatus + " " + errorThrown);
+                    try {
+                        var responseToken = jqXHR.getResponseHeader('X-UserToken') ||jqXHR.getResponseHeader('X-UserToken-Response') ;
+                        if (token != responseToken) {	
+                            // If we come here, we are getting a new token	
+                            window.localStorage.setItem('X-UserToken', responseToken);	
+    //                        console.log("Set X-UserToken in local storage to " + responseToken);	
+                        }
+                       } catch (e) {
+                           console.log("Error: "+e);
+                       }
+                       container.getSession(token);
                 })
                 .always(function (response, textStatus, jqXHR) { //;paf; see http://stackoverflow.com/a/19498463/2914328
                     /* In always because we will either have a live token in X-UserToken or a rejected transaction with 
                     X-UserToken-Response populated with new one. The work happens here not 
                     */
-                   try {
-                    var responseToken = jqXHR.getResponseHeader('X-UserToken') ||jqXHR.getResponseHeader('X-UserToken-Response') ;
-                   } catch (e) {
-                       console.log("Error: "+e);
-                   }
-                    usermenu.handleLoginStatus(response);
+
                 }
                 );
         };
